bc73a708d6cf1f9ab65c1582c3664bc6d4df41ce
yulin dima <yulin_dimon@mail.ru>
2022-04-04 21:16:40 +0300

    create theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {theme} from "./theme/theme";
+import {ThemeProvider} from '@material-ui/core/styles';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <App/>
+        </ThemeProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/theme/theme.ts
@@ -0,0 +1,93 @@
+import {createTheme} from "@material-ui/core";
+import {red} from "@material-ui/core/colors";
+
+
+export const theme = createTheme({
+    typography:{
+        fontFamily:[
+            'system-ui',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            'Segoe UI',
+            'roboto',
+            'Ubuntu',
+            'Helvetica Neue',
+            'sans-serif',
+        ] as any
+    },
+    palette:{
+        primary:{
+            main:'rgb(29,161,242)',
+            dark:'rgb(26,145,218)',
+            contrastText:'#fff',
+        },
+        secondary:{
+            main:'rgb(26,145,218)'
+        },
+        error:{
+            main: red.A400,
+        },
+        background:{
+            default:'#fff'
+        },
+    },
+    shadows:[] as any,
+    overrides:{
+        MuiButton:{
+            root:{
+               borderRadius: 30,
+               textTransform:'none',
+               fontSize:16,
+               height:40,
+            },
+            textPrimary:{
+                paddingLeft:20,
+                paddingRight:20,
+            },
+            outlinedPrimary:{
+                borderColor:'rgb(29,161,243)'
+            },
+        },
+        MuiFilledInput:{
+            underline:{
+                '&:after':{
+                    borderBottomWidth: '2px',
+                },
+                '&:before':{
+                    borderColor:'#000',
+                    borderBottomWidth:'2px'
+                },
+            },
+            input:{
+                backgroundColor: 'rgb(245,248,250)'
+            },
+        },
+        MuiDialog:{
+            paper:{
+                borderRadius:15,
+            }
+        },
+        MuiDialogActions:{
+            root:{
+                marginBottom:8,
+            },
+        },
+        MuiDialogTitle:{
+            root:{
+                borderBottom:'1px solid rgb(204,214,221)',
+                marginBottom:10,
+                padding:'10px 15px',
+                '& h2':{
+                    display:'flex',
+                    alignItems:'center',
+                    fontWeight:800,
+                },
+                '& buttom':{
+                    padding:8,
+                    marginRight:20
+                }
+
+            }
+        }
+    }
+})
