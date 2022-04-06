b61a529687d566568acc188cde3dd6a9c009ac5f
yulin dima <yulin_dimon@mail.ru>
2022-04-06 21:33:48 +0300

    fix SignIn component

--- src/component/pages/SignIn/SignIn.tsx
+++ src/component/pages/SignIn/SignIn.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react'
-import {createStyles, makeStyles, Typography} from "@material-ui/core";
+import {Button, createStyles, makeStyles, Typography} from "@material-ui/core";
 import classNames from "classnames";
-
+import TwitterIcon from '@material-ui/icons/Twitter';
 
 export const useStylesSignIn = makeStyles((theme) => ({
     wrapper: {
-        display:'flex',
-        height:'100%'
+        display: 'flex',
+        height: '100%'
     },
     leftSide: {
         backgroundColor: '#1DA1F2'
@@ -26,11 +26,17 @@ export const SignIn = () => {
                     <li>
                         <Typography>Читайте о том, что вам интересно.</Typography>
                     </li>
-                    <li>Узнайте, о чем говорят в мире.</li>
-                    <li>Присоединяйтесь к общению.</li>
+                    <li><Typography>Узнайте, о чем говорят в мире.</Typography></li>
+                    <li><Typography>Присоединяйтесь к общению.</Typography></li>
                 </ul>
             </div>
-            <div className={classNames(classes.loginSide)}></div>
+            <div className={classNames(classes.loginSide)}>
+                <TwitterIcon/>
+                <Typography>Узнайте, о чем говорят в мире.</Typography>
+                <Typography>Узнайте, о чем говорят в мире.</Typography>
+                <Button  variant={'contained'} color={'primary'} fullWidth>Зарегистрироваться</Button>
+                <Button variant={'contained'} color={'primary'} fullWidth>Войти</Button>
+            </div>
         </div>
     )
 }
