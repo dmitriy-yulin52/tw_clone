76ffe5755c67a27f679fd118bc0c92501636af8f
yulin dima <yulin_dimon@mail.ru>
2022-05-27 11:02:42 +0300

    added test for Right-side-users and Right-side-tags

--- src/component/Right-side/Right-side-users.tsx
+++ src/component/Right-side/Right-side-users.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import {memo, ReactElement} from 'react'
 import {MaterialBlock} from "../../utils/components-utils";
 import {User} from "../../store/reducers/ducks/users/types";
-import {IconButton} from "@material-ui/core";
+import {Box, IconButton} from "@material-ui/core";
 import PersonAddIcon from "@material-ui/icons/PersonAdd";
 import {Link} from "react-router-dom";
 
@@ -11,22 +11,25 @@ interface RightSideUsersProps {
 }
 
 const styleLink = {
-    textDecoration:'none'
-}as const
+    textDecoration: 'none'
+} as const
 
-export const RightSideUsers = memo((props: RightSideUsersProps): ReactElement => {
+export const RightSideUsers = memo((props: RightSideUsersProps): ReactElement | null => {
     const {users} = props
 
-    return <> {users.map((user, index) => (
-        <Link style={styleLink} to={`home/search?q=${user.name}`}><MaterialBlock
+    if (!users.length) return null
+
+
+    return <Box  key={users.length}> {users.map((user, index) => (
+        <Link style={styleLink} to={`home/search?q=${user.name}`} key={users.length - index}><MaterialBlock
             style
             fullName={user.name}
             userName={user.userName}
-            key={users.length - index}
+
             avatarUrl={user.avatarUrl}
             headerButton={
                 <IconButton color={"primary"}
                 ><PersonAddIcon/></IconButton>}/>
-        </Link >
-    ))}</>
+        </Link>
+    ))}</Box>
 })
